4fb967d2b91a6304ec28bc8f45670ee8ef429afd
Vandita Mamgain <vandita.mamgain@gmail.com>
2026-03-04 02:47:03 +0530

    Added UC-3 implementation

---
+++ UC-3.java
@@ -0,0 +1,13 @@
+public class UC-3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("  ", " ***** ", "  ", " ***** ", "  ", "****** ", "  ", " ******"));
+        System.out.println(String.join("  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "));
+        System.out.println(String.join("  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "));
+        System.out.println(String.join("  ", "*     *", "  ", "*     *", "  ", "****** ", "  ", " ***** "));
+        System.out.println(String.join("  ", "*     *", "  ", "*     *", "  ", "*      ", "  ", "      *"));
+        System.out.println(String.join("  ", "*     *", "  ", "*     *", "  ", "*     ","   ","*        *"));
+        System.out.println(String.join("  ", " ***** ","  "," ***** ","  ","*      ","  "," ******"));
+
+    }
+}
