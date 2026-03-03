133db50a5bd441d6c35bc2e0add3da0eb70ea302
Vandita Mamgain <vandita.mamgain@gmail.com>
2026-03-04 02:34:50 +0530

    Added UC-2 Implemntation

---
+++ UC-1.java
@@ -0,0 +1,9 @@
+/**
+ * OOPS BANNER APP UC-1 - OOPS Banner Display Application
+* This application displays a banner message "OOPS Banner" to the console.
+ */
+public class UC-1{
+  public static void main(String[]args){
+    System.out.println("OOPS Banner");
+  }
+}
