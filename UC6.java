6153cb9f0f178c82442439a7a5216bf42f5e8edc
Vandita Mamgain <vandita.mamgain@gmail.com>
2026-03-07 19:24:09 +0530

    Implemented UC-6

---
+++ UC6.java
@@ -0,0 +1,54 @@
+public class UC6 {
+
+    // Method to generate pattern for 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Method to generate pattern for 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method to generate pattern for 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Print banner O O P S
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + "  " + oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
+        }
+    }
+}
